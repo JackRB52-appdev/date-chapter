2a1704bdae463f03659bfe9a0400bf9df91e7fb3
JackRB52 <jbouchard@chicagobooth.edu>
2022-04-13 21:31:43 +0000

    playing with dates

--- date_formatted.rb
+++ date_formatted.rb
@@ -1,3 +1,8 @@
 # Should format and identify different parts of todays date.
 # 
 # "The year is: 2022, the calendar day is: 1, and the month is: 7."
+require "date"
+
+current_day = Date.today
+
+p "The year is: #{current_day.year}, the calendar day is: #{current_day.day}, and the month is: #{current_day.month}."
--- date_math.rb
+++ date_math.rb
@@ -5,3 +5,8 @@
 # Output:
 #   "Ruby is 108937 days old!"
 #
+require "date"
+
+date_diff = Date.today - Date.new(1995,12,21)
+date_diff = date_diff.to_i
+p "Ruby is #{date_diff} days old!"
